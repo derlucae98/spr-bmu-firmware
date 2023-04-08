a79b484be08c7cca6a0758f3814d44af3548d739
Luca Engelmann <lucaengelmann@posteo.de>
2023-04-08 16:55:51 +0200

    LTC6811: Add file info and license to header file

--- include/LTC6811.h
+++ include/LTC6811.h
@@ -1,3 +1,31 @@
+/*!
+ * @file            LTC6811.h
+ * @brief           Library for the LTC6811 battery stack monitor
+ *                  This Library is designed for a specific hardware.
+ */
+
+/*
+Copyright 2023 Luca Engelmann
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #ifndef LTC6811_H_
 #define LTC6811_H_
 
--- src/LTC6811.c
+++ src/LTC6811.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 Luca Engelmann
+Copyright 2023 Luca Engelmann
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the "Software"),
