22fe73e604c9cff4f57c517a9f33a5d23066c26a
Luca Engelmann <lucaengelmann@posteo.de>
2023-04-14 21:21:14 +0200

    FreeRTOS: "Give me more RAM"

--- include/FreeRTOSConfig.h
+++ include/FreeRTOSConfig.h
@@ -54,7 +54,7 @@
 #define configTICK_RATE_HZ				( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES			( 9 )
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 150 )
-#define configTOTAL_HEAP_SIZE			( ( size_t ) (80000) )
+#define configTOTAL_HEAP_SIZE			( ( size_t ) (240000) )
 #define configMAX_TASK_NAME_LEN			( 10 )
 #define configUSE_TRACE_FACILITY		0
 #define configUSE_16_BIT_TICKS			0
