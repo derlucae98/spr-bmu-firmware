757a666cd9d438cf610c36a25d52086067db69b5
Luca Engelmann <lucaengelmann@posteo.de>
2023-04-15 21:35:04 +0200

    Contactor: State machine documented and partially improved
    Multiple error reasons can be set to be more verbose.

--- include/contactor.h
+++ include/contactor.h
@@ -8,15 +8,16 @@
 #ifndef CONTACTOR_H_
 #define CONTACTOR_H_
 
-#include "adc.h"
 #include "S32K14x.h"
 #include "FreeRTOS.h"
 #include "task.h"
 #include "gpio.h"
 #include "config.h"
-#include <stdbool.h>
 #include "wdt.h"
 #include "math.h"
+#include "uart.h"
+#include "adc.h"
+#include <stdbool.h>
 
 
 typedef enum {
@@ -36,12 +37,31 @@ typedef enum {
 } state_t;
 
 typedef enum {
-    ERROR_NO_ERROR,
-    ERROR_SYSTEM_NOT_HEALTHY,
-    ERROR_CONTACTOR_IMPLAUSIBLE,
-    ERROR_PRE_CHARGE_TOO_SHORT,
-    ERROR_PRE_CHARGE_TIMEOUT
+    ERROR_NO_ERROR                    = 0x0,
+    ERROR_SYSTEM_NOT_HEALTHY          = 0x1,
+    ERROR_IMD_FAULT                   = 0x2,
+    ERROR_AMS_FAULT                   = 0x4,
+    ERROR_IMPLAUSIBLE_CONTACTOR       = 0x8,
+    ERROR_IMPLAUSIBLE_DC_LINK_VOLTAGE = 0x10,
+    ERROR_IMPLAUSIBLE_BATTERY_VOLTAGE = 0x20,
+    ERROR_PRE_CHARGE_TIMEOUT          = 0x40,
+    ERROR_SDC_OPEN                    = 0x80,
+    ERROR_AMS_POWERSTAGE_DISABLED     = 0x100,
+    ERROR_IMD_POWERSTAGE_DISABLED     = 0x200
 } error_t;
+
+
+typedef struct {
+    uint8_t negAIR_intent;
+    uint8_t negAIR_actual;
+    uint8_t negAIR_isPlausible;
+    uint8_t posAIR_intent;
+    uint8_t posAIR_actual;
+    uint8_t posAIR_isPlausible;
+    uint8_t pre_intent;
+    uint8_t pre_actual;
+    uint8_t pre_isPlausible;
+} contactor_state_t;
 
 state_t get_contactor_state(void);
 error_t get_contactor_error(void);
--- src/contactor.c
+++ src/contactor.c
@@ -1,32 +1,49 @@
+/*!
+ * @file            contactor.c
+ * @brief           This module controls the Accumulator Isolation Relays (AIRs)
+ */
+
 /*
- * contactor.c
- *
- *  Created on: Feb 10, 2022
- *      Author: Luca Engelmann
+Copyright 2023 Luca Engelmann
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 #include "contactor.h"
 
-extern void PRINTF(const char *format, ...);
+static void prv_contactor_control_task(void *p);
 
-static void contactor_control_task(void *p);
+static void prv_standby(void);
+static void prv_pre_charge(void);
+static void prv_operate(void);
+static void prv_error(void);
 
-static void standby(void);
-static void pre_charge(void);
-static void operate(void);
-static void error(void);
-
-static bool _tsActive = false;
-static uint8_t _tsRequestTimeout = 0;
-static uint8_t _contactorStateMachineState = STATE_STANDBY;
-static uint8_t _contactorStateMachineError = ERROR_NO_ERROR;
-
+static bool prvTsActive = false;
+static uint8_t prvTsRequestTimeout = 0;
+static uint8_t prvPrechargeTimeout = 0;
+static uint8_t prvStateMachineState = STATE_STANDBY;
+static uint32_t prvStateMachineError = ERROR_NO_ERROR;
+static contactor_state_t prvContactorState;
 
 typedef struct {
     state_t current;
 } state_machine_t;
-
-
 
 typedef struct {
     state_t current;
@@ -34,7 +51,7 @@ typedef struct {
     state_t next;
 } state_array_t;
 
-static state_array_t stateArray[] = {
+static state_array_t prvStateArray[] = {
     {STATE_STANDBY,    EVENT_TS_ACTIVATE,           STATE_PRE_CHARGE},
     {STATE_STANDBY,    EVENT_ERROR,                 STATE_ERROR},
     {STATE_PRE_CHARGE, EVENT_TS_DEACTIVATE,         STATE_STANDBY},
@@ -50,15 +67,15 @@ typedef struct {
     void (*function)(void);
 } state_function_t;
 
-static state_function_t stateFunction[] = {
-    {"STATE_STANDBY",    &standby},
-    {"STATE_PRE_CHARGE", &pre_charge},
-    {"STATE_OPERATE",    &operate},
-    {"STATE_ERROR",      &error}
+static state_function_t prvStateFunction[] = {
+    {"STATE_STANDBY",    &prv_standby},
+    {"STATE_PRE_CHARGE", &prv_pre_charge},
+    {"STATE_OPERATE",    &prv_operate},
+    {"STATE_ERROR",      &prv_error}
 };
 
-static state_machine_t _stateMachine;
-static event_t _contactorEvent = EVENT_NONE;
+static state_machine_t prvStateMachine;
+static event_t prvEvent = EVENT_NONE;
 
 typedef enum {
     CONTACTOR_HV_POS,
@@ -66,7 +83,7 @@ typedef enum {
     CONTACTOR_HV_PRE
 } contactor_t;
 
-static inline void open_contactor(contactor_t contactor) {
+static inline void prv_open_contactor(contactor_t contactor) {
     switch (contactor) {
         case CONTACTOR_HV_POS:
             clear_pin(AIR_POS_SET_PORT, AIR_POS_SET_PIN);
@@ -83,7 +100,7 @@ static inline void open_contactor(contactor_t contactor) {
     }
 }
 
-static inline void close_contactor(contactor_t contactor) {
+static inline void prv_close_contactor(contactor_t contactor) {
     switch (contactor) {
         case CONTACTOR_HV_POS:
             set_pin(AIR_POS_CLR_PORT, AIR_POS_CLR_PIN);
@@ -103,42 +120,44 @@ static inline void close_contactor(contactor_t contactor) {
     }
 }
 
-static inline void open_all_contactors(void) {
-    open_contactor(CONTACTOR_HV_POS);
-    open_contactor(CONTACTOR_HV_NEG);
-    open_contactor(CONTACTOR_HV_PRE);
+static inline void prv_open_all_contactors(void) {
+    prv_open_contactor(CONTACTOR_HV_POS);
+    prv_open_contactor(CONTACTOR_HV_NEG);
+    prv_open_contactor(CONTACTOR_HV_PRE);
 }
 
-static void standby(void) {
-    open_all_contactors();
-//    PRINTF("State standby\n");
+static void prv_standby(void) {
+    prv_open_all_contactors();
+    prvPrechargeTimeout = 0;
+    PRINTF("State standby\n");
 }
 
-static void pre_charge(void) {
-    open_contactor(CONTACTOR_HV_POS);
-    close_contactor(CONTACTOR_HV_PRE);
-    close_contactor(CONTACTOR_HV_NEG);
-//    PRINTF("State pre-charge\n");
+static void prv_pre_charge(void) {
+    prv_open_contactor(CONTACTOR_HV_POS);
+    prv_close_contactor(CONTACTOR_HV_PRE);
+    prv_close_contactor(CONTACTOR_HV_NEG);
+    PRINTF("State pre-charge\n");
 }
 
-static void operate(void) {
-    close_contactor(CONTACTOR_HV_POS);
-    close_contactor(CONTACTOR_HV_NEG);
-    open_contactor(CONTACTOR_HV_PRE);
-//    PRINTF("State operate\n");
+static void prv_operate(void) {
+    prv_close_contactor(CONTACTOR_HV_POS);
+    prv_close_contactor(CONTACTOR_HV_NEG);
+    prv_open_contactor(CONTACTOR_HV_PRE);
+    PRINTF("State operate\n");
 }
 
-static void error(void) {
-    open_all_contactors();
-//    PRINTF("State error\n");
+static void prv_error(void) {
+    prv_open_all_contactors();
+    PRINTF("State error\n");
+    PRINTF("Reason: %d\n", prvStateMachineError);
 }
 
 void init_contactor(void) {
-    _stateMachine.current = STATE_STANDBY;
-    xTaskCreate(contactor_control_task, "contactor", CONTACTOR_TASK_STACK, NULL, CONTACTOR_TASK_PRIO, NULL);
+    prvStateMachine.current = STATE_STANDBY;
+    xTaskCreate(prv_contactor_control_task, "contactor", CONTACTOR_TASK_STACK, NULL, CONTACTOR_TASK_PRIO, NULL);
 }
 
-static void contactor_control_task(void *p) {
+static void prv_contactor_control_task(void *p) {
     (void) p;
     TickType_t xLastWakeTime;
     const TickType_t xPeriod = pdMS_TO_TICKS(100);
@@ -148,6 +167,30 @@ static void contactor_control_task(void *p) {
 
     while (1) {
         refresh_wdt();
+
+        //Get intended and actual AIR states
+        prvContactorState.negAIR_intent      = get_pin(AIR_NEG_INTENT_PORT, AIR_NEG_INTENT_PIN);
+        prvContactorState.negAIR_actual      = get_pin(AIR_NEG_STATE_PORT,  AIR_NEG_STATE_PIN);
+        prvContactorState.posAIR_intent      = get_pin(AIR_POS_INTENT_PORT, AIR_POS_INTENT_PIN);
+        prvContactorState.posAIR_actual      = get_pin(AIR_POS_STATE_PORT,  AIR_POS_STATE_PIN);
+        prvContactorState.pre_intent         = get_pin(AIR_PRE_INTENT_PORT, AIR_PRE_INTENT_PIN);
+        prvContactorState.pre_actual         = get_pin(AIR_PRE_STATE_PORT,  AIR_PRE_STATE_PIN);
+
+        prvContactorState.negAIR_isPlausible = prvContactorState.negAIR_intent == prvContactorState.negAIR_actual;
+        prvContactorState.posAIR_isPlausible = prvContactorState.posAIR_intent == prvContactorState.posAIR_actual;
+        prvContactorState.pre_isPlausible    = prvContactorState.pre_intent    == prvContactorState.pre_actual;
+
+        //AIR plausibility combined
+        bool airPlausible = prvContactorState.negAIR_isPlausible && prvContactorState.posAIR_isPlausible && prvContactorState.pre_isPlausible;
+
+        //Get battery and DC-Link voltage
+        adc_data_t adcData;
+        bool cpyret = copy_adc_data(&adcData, pdMS_TO_TICKS(200));
+        if (cpyret == false) {
+            prv_open_all_contactors();
+            configASSERT(0);
+        }
+
         /*
          * 1) System is healthy if AMS and IMD are not in error state,
          * 2) AMS and IMD power stages are enabled to close the shutdown circuit,
@@ -155,70 +198,100 @@ static void contactor_control_task(void *p) {
          * 4) Relay state is plausible
          */
         bool systemIsHealthy = true;
-        bool relayPlausible = true;
         bool voltageEqual = false;
 
-        uint8_t contactorState = 0; //bit-coded: 00000xyz, x=pos, y=neg, z=pre
-        bool stateNegAir = false;
-        bool statePosAir = false;
-        bool statePre = false;
+        prvStateMachineState = prvStateMachine.current;
 
-        _contactorStateMachineState = _stateMachine.current;
-
-
-        adc_data_t *adcData = get_adc_data(portMAX_DELAY);
-        if (adcData != NULL) {
-            if (fabs(adcData->batteryVoltage - adcData->dcLinkVoltage) <= (0.05f * adcData->batteryVoltage)) {
-                voltageEqual = true;
-            }
-            release_adc_data();
-        }
-//        if (++_tsRequestTimeout >= 5) {
-//            //timeout after 500ms
-//            _tsActive = false;
-//        }
-
-        _contactorEvent = EVENT_NONE;
-
-
-
-        if ((_stateMachine.current == STATE_PRE_CHARGE) && voltageEqual) {
-            _contactorEvent = EVENT_PRE_CHARGE_SUCCESSFUL;
-        }
-        if (_stateMachine.current == STATE_PRE_CHARGE && !_tsActive) {
-            _contactorEvent = EVENT_TS_DEACTIVATE;
+        /* Pre-charge sequence:
+         * DC-Link voltage has to reach at least 95% of the battery voltage */
+        if (fabs(adcData.batteryVoltage - adcData.dcLinkVoltage) <= (0.05f * adcData.batteryVoltage)) {
+            voltageEqual = true;
         }
 
-        if (_stateMachine.current == STATE_STANDBY && _tsActive) {
-            _contactorEvent = EVENT_TS_ACTIVATE;
+        /* Pre-charge timeout counter */
+        if (prvStateMachine.current == STATE_PRE_CHARGE && !voltageEqual) {
+            prvPrechargeTimeout++;
         }
 
-        if (_stateMachine.current == STATE_OPERATE && !_tsActive) {
-            _contactorEvent = EVENT_TS_DEACTIVATE;
+        /* TS request is automatically withdrawn if the CAN message is absent for more than 500 ms */
+        if (++prvTsRequestTimeout >= 5) {
+            prvTsActive = false;
         }
 
-        //Clear error if TS request clears and errors are gone
-        if (_stateMachine.current == STATE_ERROR && !_tsActive) {
-            _contactorEvent = EVENT_ERROR_CLEARED;
-            _contactorStateMachineError = ERROR_NO_ERROR;
+        /* Initialize event */
+        prvEvent = EVENT_NONE;
+
+        /* Waiting for pre-charge and voltages are equal?
+         * -> Pre-charge was successful */
+        if ((prvStateMachine.current == STATE_PRE_CHARGE) && voltageEqual) {
+            prvEvent = EVENT_PRE_CHARGE_SUCCESSFUL;
         }
+
+        /* Waiting for pre-charge and TS activation request has been withdrawn?
+         * -> Deactivate tractive system */
+        if (prvStateMachine.current == STATE_PRE_CHARGE && !prvTsActive) {
+            prvEvent = EVENT_TS_DEACTIVATE;
+        }
+
+        /* Pre-charge timed out?
+         * -> Possibly a short-circuit at the TSAC output */
+        if (prvStateMachine.current == STATE_PRE_CHARGE && prvPrechargeTimeout >= PRECHARGE_TIMEOUT) {
+            prvEvent = EVENT_ERROR;
+            prvStateMachineError |= ERROR_PRE_CHARGE_TIMEOUT;
+        }
+
+        /* Standby and TS activation has been requested?
+         * -> Active tractive system with pre-charge sequence */
+        if (prvStateMachine.current == STATE_STANDBY && prvTsActive) {
+            prvEvent = EVENT_TS_ACTIVATE;
+        }
+
+        /* TS is active and activation request has been withdrawn?
+         * -> Deactivate tractive system */
+        if (prvStateMachine.current == STATE_OPERATE && !prvTsActive) {
+            prvEvent = EVENT_TS_DEACTIVATE;
+        }
+
+        /* DC-Link voltage lower than 80% of the minimum battery voltage and contactors are active?
+         * -> DC-Link voltage measurement broken wire */
+        if (prvStateMachine.current == STATE_OPERATE && adcData.dcLinkVoltage < (0.8f * MIN_BATTERY_VOLTAGE)) {
+            prvEvent = EVENT_ERROR;
+            prvStateMachineError |= ERROR_IMPLAUSIBLE_DC_LINK_VOLTAGE;
+        }
+
+        /* Battery voltage lower than 80% of the minimum battery voltage and contactors are active?
+         * -> Battery voltage measurement broken wire OR battery depleted */
+        if (prvStateMachine.current == STATE_OPERATE && adcData.batteryVoltage < (0.8f * MIN_BATTERY_VOLTAGE)) {
+            prvEvent = EVENT_ERROR;
+            prvStateMachineError |= ERROR_IMPLAUSIBLE_BATTERY_VOLTAGE;
+        }
+
+        /* Error state is active and TS request gets withdrawn?
+         * -> This is considered as error reset. */
+        if (prvStateMachine.current == STATE_ERROR && !prvTsActive) {
+            prvEvent = EVENT_ERROR_CLEARED;
+            prvStateMachineError = ERROR_NO_ERROR;
+        }
+
+
+
 
         if (!systemIsHealthy) {
-            _contactorEvent = EVENT_ERROR;
-            _contactorStateMachineError = ERROR_SYSTEM_NOT_HEALTHY;
+            prvEvent = EVENT_ERROR;
+            prvStateMachineError |= ERROR_SYSTEM_NOT_HEALTHY;
         }
-        if (!relayPlausible) {
-            _contactorEvent = EVENT_ERROR;
-            _contactorStateMachineError = ERROR_CONTACTOR_IMPLAUSIBLE;
+        if (!airPlausible) {
+            prvEvent = EVENT_ERROR;
+            prvStateMachineError |= ERROR_IMPLAUSIBLE_CONTACTOR;
         }
 
-        for(size_t i = 0; i < sizeof(stateArray)/sizeof(stateArray[0]); i++) {
-           if(stateArray[i].current == _stateMachine.current) {
-                if((stateArray[i].event == _contactorEvent)) {
+        for(size_t i = 0; i < sizeof(prvStateArray)/sizeof(prvStateArray[0]); i++) {
+           if(prvStateArray[i].current == prvStateMachine.current) {
+                if((prvStateArray[i].event == prvEvent)) {
 
-                    _stateMachine.current =  stateArray[i].next;
+                    prvStateMachine.current =  prvStateArray[i].next;
 
-                    stateFunction[_stateMachine.current].function();
+                    prvStateFunction[prvStateMachine.current].function();
                 }
             }
         }
@@ -227,17 +300,17 @@ static void contactor_control_task(void *p) {
 }
 
 void request_tractive_system(bool active) {
-    _tsActive = active;
+    prvTsActive = active;
     if (active) {
-        _tsRequestTimeout = 0;
+        prvTsRequestTimeout = 0;
     }
 }
 
 state_t get_contactor_state(void) {
-    return _contactorStateMachineState;
+    return prvStateMachineState;
 }
 error_t get_contactor_error(void) {
-    return _contactorStateMachineError;
+    return prvStateMachineError;
 }
 
 
